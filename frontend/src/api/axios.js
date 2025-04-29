e708e9375f6c7e27de3877d1c735edbcf8e04412
karthi <karthikeyanks463@gmail.com>
2025-04-29 11:34:39 +0530

    Changed API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://food-hub-backend-t31h.onrender.com/api',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 // You can auto attach token if needed
